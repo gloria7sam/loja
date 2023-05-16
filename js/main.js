1ed8c3db08932f03b0b6e56aa205944a68b1bdf5
Glória Samuel <gloriasamuel789@gmail.com>
2023-05-16 14:09:26 +0100

    Alterações 16/05/2023

--- js/main.js
+++ js/main.js
@@ -21,3 +21,65 @@ function topFunction() {
 }
 
 
+function orcamento(){
+
+  nome = document.getElementById('pnome').value;
+  email = document.getElementById('mail').value;
+  descricao = document.getElementById('descricao').value;
+
+  if(nome == "" || email == "" || descricao == ""){
+
+    Swal.fire({
+        title: Atenção,
+        text: "Por favor preencha os campos",
+        timer: 3500
+      
+      }
+    )
+    
+  } else{
+    alert("Pedido de Orçamento enviado com sucesso")
+    
+  }
+}
+
+
+function alerta(){
+  Swal.fire({
+    title: Atenção,
+    text: "Por favor preencha os campos",
+    timer: 3500
+  
+  }
+)
+}
+
+function novidades(){
+  
+  let novidades = document.getElementById('novidades');
+  let tudo = document.getElementById('tudo');
+
+  if(novidades != ""){
+    document.getElementById("div-3").style.display = "none";
+    document.getElementById("div-4").style.display = "none";
+    document.getElementById("div-5").style.display = "none";
+    document.getElementById("div-6").style.display = "none";
+}
+
+}
+
+
+function mostrarTodos() {
+  document.getElementById("div-3").style.display="block";
+  document.getElementById("div-4").style.display="block";
+  document.getElementById("div-5").style.display="block";
+  document.getElementById("div-6").style.display="block";
+}
+
+function maisBarato(){
+  let price = document.getElementById("price").value;
+
+  if(price > price - 1){
+    document.getElementById("price").value;
+  }
+}
